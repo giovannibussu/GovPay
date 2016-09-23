776e02725698e47ede469af00e5c3e161a75072b
Giovanni Bussu <bussu@link.it>
2016-09-23 17:41:50 +0200

    patch per postgresql

--- resources/db/sql/postgresql/patch/2.2.1.sql
+++ resources/db/sql/postgresql/patch/2.2.1.sql
@@ -1,17 +1,17 @@
 --GP-169
-DROP CONSTRAINT fk_versamenti_1;
-DROP CONSTRAINT fk_versamenti_2;
-DROP CONSTRAINT fk_singoli_versamenti_1;
-DROP CONSTRAINT fk_singoli_versamenti_2;
-DROP CONSTRAINT fk_singoli_versamenti_3;
-DROP CONSTRAINT fk_rpt_1;
-DROP CONSTRAINT fk_rpt_2;
-DROP CONSTRAINT fk_rpt_3;
-DROP CONSTRAINT fk_notifiche_1;
-DROP CONSTRAINT fk_notifiche_2;
-DROP CONSTRAINT fk_notifiche_3;
-DROP CONSTRAINT fk_iuv_1;
-DROP CONSTRAINT fk_iuv_2;
+ALTER TABLE versamenti DROP CONSTRAINT fk_versamenti_1;
+ALTER TABLE versamenti DROP CONSTRAINT fk_versamenti_2;
+ALTER TABLE singoli_versamenti DROP CONSTRAINT fk_singoli_versamenti_1;
+ALTER TABLE singoli_versamenti DROP CONSTRAINT fk_singoli_versamenti_2;
+ALTER TABLE singoli_versamenti DROP CONSTRAINT fk_singoli_versamenti_3;
+ALTER TABLE rpt DROP CONSTRAINT fk_rpt_1;
+ALTER TABLE rpt DROP CONSTRAINT fk_rpt_2;
+ALTER TABLE rpt DROP CONSTRAINT fk_rpt_3;
+ALTER TABLE notifiche DROP CONSTRAINT fk_notifiche_1;
+ALTER TABLE notifiche DROP CONSTRAINT fk_notifiche_2;
+ALTER TABLE notifiche DROP CONSTRAINT fk_notifiche_3;
+ALTER TABLE iuv DROP CONSTRAINT fk_iuv_1;
+ALTER TABLE iuv DROP CONSTRAINT fk_iuv_2;
 
 CREATE INDEX index_iuv_1 ON iuv (cod_versamento_ente,tipo_iuv,id_applicazione);
 
