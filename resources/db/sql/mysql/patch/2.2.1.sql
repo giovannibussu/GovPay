721ac521c81afc0903118112dfb1814942a34df7
Giovanni Bussu <bussu@link.it>
2016-09-29 17:58:59 +0200

    corretta patch mysql

--- resources/db/sql/mysql/patch/2.2.1.sql
+++ resources/db/sql/mysql/patch/2.2.1.sql
@@ -1,17 +1,17 @@
 --GP-169
-ALTER TABLE versamenti DROP CONSTRAINT fk_versamenti_1;
-ALTER TABLE versamenti DROP CONSTRAINT fk_versamenti_2;
-ALTER TABLE singoli_versamenti DROP CONSTRAINT fk_singoli_versamenti_1;
-ALTER TABLE singoli_versamenti DROP CONSTRAINT fk_singoli_versamenti_2;
-ALTER TABLE singoli_versamenti DROP CONSTRAINT fk_singoli_versamenti_3;
-ALTER TABLE rpt DROP CONSTRAINT fk_rpt_1;
-ALTER TABLE rpt DROP CONSTRAINT fk_rpt_2;
-ALTER TABLE rpt DROP CONSTRAINT fk_rpt_3;
-ALTER TABLE notifiche DROP CONSTRAINT fk_notifiche_1;
-ALTER TABLE notifiche DROP CONSTRAINT fk_notifiche_2;
-ALTER TABLE notifiche DROP CONSTRAINT fk_notifiche_3;
-ALTER TABLE iuv DROP CONSTRAINT fk_iuv_1;
-ALTER TABLE iuv DROP CONSTRAINT fk_iuv_2;
+ALTER TABLE versamenti DROP foreign key fk_versamenti_1;
+ALTER TABLE versamenti DROP foreign key fk_versamenti_2;
+ALTER TABLE singoli_versamenti DROP foreign key fk_singoli_versamenti_1;
+ALTER TABLE singoli_versamenti DROP foreign key fk_singoli_versamenti_2;
+ALTER TABLE singoli_versamenti DROP foreign key fk_singoli_versamenti_3;
+ALTER TABLE rpt DROP foreign key fk_rpt_1;
+ALTER TABLE rpt DROP foreign key fk_rpt_2;
+ALTER TABLE rpt DROP foreign key fk_rpt_3;
+ALTER TABLE notifiche DROP foreign key fk_notifiche_1;
+ALTER TABLE notifiche DROP foreign key fk_notifiche_2;
+ALTER TABLE notifiche DROP foreign key fk_notifiche_3;
+ALTER TABLE iuv DROP foreign key fk_iuv_1;
+ALTER TABLE iuv DROP foreign key fk_iuv_2;
 
-CREATE INDEX index_iuv_1 ON iuv (cod_versamento_ente,tipo_iuv,id_applicazione);
+CREATE INDEX index_iuv_2 ON iuv (cod_versamento_ente,tipo_iuv,id_applicazione);
 
