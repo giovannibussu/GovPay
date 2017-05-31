9f7aad9adbe293c6a976be8a1ba0ca8b58aa48b4
Ivan Martoccia <ivan.martoccia@finance-evolution.com>
2017-05-31 17:05:22 +0200

    Cambio versione java da 6 a 7

--- resources/db/sql/init.sql
+++ resources/db/sql/init.sql
@@ -1,4 +1,4 @@
-INSERT INTO operatori (principal,profilo,nome) VALUES ('@PRINCIPAL@','A','@RAGIONE_SOCIALE@');
+INSERT INTO operatori (principal,profilo,nome) VALUES ('admin','A','Amministratore');
 INSERT INTO tipi_tributo (cod_tributo, descrizione) VALUES ('BOLLOT', 'Marca da Bollo Telematica');
 insert into sonde(nome, classe, soglia_warn, soglia_error) values ('update-psp', 'org.openspcoop2.utils.sonde.impl.SondaBatch', 86400000, 172800000);
 insert into sonde(nome, classe, soglia_warn, soglia_error) values ('update-rnd', 'org.openspcoop2.utils.sonde.impl.SondaBatch', 18000000, 86400000);
