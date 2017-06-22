2ff423cf3ad2780dc2d188bec60aa7af5cdafcf8
Ivan Martoccia <ivan.martoccia@finance-evolution.com>
2017-06-22 12:11:26 +0200

    Creazione Patch versione 2.x.x per Mysql Oracle e Postgres Aggiunta delle tabelle "avvisi_digitali" e "avvisi_digitali_esiti" per la gestione del servizio di Avvisatura Digitale. Modifica dei file delete e drop con aggiunta delle due tabelle.

--- resources/db/sql/mysql/patch/2.x.x.sql
+++ resources/db/sql/mysql/patch/2.x.x.sql
@@ -1,57 +1,45 @@
-ALTER TABLE domini ADD COLUMN avvisi_digitali TINYINT(1) NOT NULL AFTER custom_iuv;
-
+ALTER TABLE domini ADD COLUMN avvisi_digitali TINYINT(1) NOT NULL DEFAULT 0 AFTER custom_iuv;
 
 CREATE TABLE avvisi_digitali
 (
-	id_dominio BIGINT NOT NULL, -- Codice fiscale Ente Creditore
-	anagrafica_beneficiario VARCHAR(35) NOT NULL, -- Denominazione ente creditore
-	id_messaggio_richiesta VARCHAR(20) NOT NULL, -- Identificativo univoco nell'arco di 365gg
-	id_tributo BIGINT, -- Macro categoria avviso (tassonomia_avviso)
-	iuv VARCHAR(35) NOT NULL, -- IUV
+    id_messaggio_richiesta VARCHAR(20) NOT NULL, -- Identificativo univoco in assoluto (primi 4 caratteri occupati dall'anno)
+	id_dominio BIGINT NOT NULL, -- FK dominio
+	codice_avviso VARCHAR(18) NOT NULL, -- numero avviso
+	stato VARCHAR(35) NOT NULL,
+	descrizione_stato VARCHAR(255),
+	importo_avviso DOUBLE NOT NULL,
+	descrizione_pagamento VARCHAR(140) NOT NULL,
+	tassonomia VARCHAR(2) NOT NULL,
 	anagrafica_pagatore VARCHAR(70) NOT NULL, -- Ragione Sociale Soggetto Pagatore
 	tipo_identificativo_univoco VARCHAR(1) NOT NULL, -- F = Persona Fisica / G = Persona Giuridica
 	codice_identificativo_univoco VARCHAR(35) NOT NULL, -- Codice Fiscale / P.IVA Soggetto Pagatore
-	data_scadenza_pagamento TIMESTAMP(3) NOT NULL, -- Deve essere la stessa del versamento
-	data_scadenza_avviso TIMESTAMP(3) NOT NULL,
-	importo_avviso DOUBLE NOT NULL,
 	email_soggetto_pagatore VARCHAR(255), -- NULLABLE Se presente viene inviata una mail al soggetto con le indicazioni per effettuare il pagamento
 	cellulare_soggetto_pagatore VARCHAR(35), -- NULLABLE Se presente viene inviato un sms al soggetto  con le indicazioni per effettuare il pagamento
-	descrizione_pagamento VARCHAR(140),
-	url_avviso VARCHAR(140), -- NULLABLE Url della pagina sulla quale il soggetto pagatore protrà consultare l'avviso
-	id_versamento BIGINT NOT NULL,
-	stato VARCHAR(35) NOT NULL,
-	descrizione_stato VARCHAR(255),
+	data_scadenza_pagamento TIMESTAMP(3) NOT NULL, -- Deve essere la stessa del versamento
+	data_scadenza_avviso TIMESTAMP(3) NOT NULL,
 	data_creazione TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-	id_uo BIGINT NOT NULL,
+	url_avviso VARCHAR(140), -- NULLABLE Url della pagina sulla quale il soggetto pagatore protrà consultare l'avviso
 	id BIGINT AUTO_INCREMENT,
 	-- unique constraints
-	CONSTRAINT unique_avvisi_digitali_1 UNIQUE (id_messaggio_richiesta,id_dominio,id_versamento),
+	CONSTRAINT unique_avvisi_digitali_1 UNIQUE (id_messaggio_richiesta),
+	CONSTRAINT unique_avvisi_digitali_2 UNIQUE (id_dominio, codice_avviso),
 	-- fk/pk keys constraints
-	-- CONSTRAINT fk_avvisi_digitali_1 FOREIGN KEY (id_uo) REFERENCES uo(id),
-	-- CONSTRAINT fk_avvisi_digitali_2 FOREIGN KEY (id_versamento) REFERENCES versamenti(id),
-	-- CONSTRAINT fk_avvisi_digitali_3 FOREIGN KEY (id_tributo) REFERENCES tributi(id),
-	-- CONSTRAINT fk_avvisi_digitali_4 FOREIGN KEY (id_dominio) REFERENCES domini(id),
+	CONSTRAINT fk_avvisi_digitali_1 FOREIGN KEY (id_dominio) REFERENCES domini(id),
 	CONSTRAINT pk_avvisi_digitali PRIMARY KEY (id)
 )ENGINE INNODB CHARACTER SET latin1 COLLATE latin1_general_cs;
 
--- index
-CREATE INDEX index_avvisi_digitali_1 ON avvisi_digitali (id_messaggio_richiesta,id_dominio,id_versamento);
-
-
 CREATE TABLE avvisi_digitali_esiti
 (
-	id_dominio BIGINT NOT NULL, -- Codice fiscale Ente Creditore
-	id_messaggio_richiesta VARCHAR(20) NOT NULL, -- Identificativo univoco nell'arco di 365gg
+	id_avviso_digitale BIGINT NOT NULL, -- FK all'avviso digitale
+	identificativo_canale VARCHAR(35), -- Identificativo del canale mobile a cui si riferisce l'esito dell'avviso. Presente solo se tipo_canale_esito = 2/4
 	tipo_canale_esito BIGINT NOT NULL, -- 0 = Nessun Canale / 1 = SMS / 2 = E-Mail / 3 = Mobile Payment / 4 = Altro canale psp
-	identificativo_canale VARCHAR(35) NOT NULL, -- Identificativo del canale mobile a cui si riferisce l'esito dell'avviso. Presente solo se tipo_canale_esito = 2/4
 	data_esito TIMESTAMP(3) NOT NULL,
 	codice_esito BIGINT NOT NULL, -- Esito del singolo canale di invio 0 = OK / 1 = KO / N = Altri esiti da definire
-	descrizione_esito VARCHAR(140) NOT NULL, -- Descrizione dell'esito dell'operazione in caso di codice_esito <> 0
-	id_avviso_digitale BIGINT NOT NULL,
+	descrizione_esito VARCHAR(140), -- Descrizione dell'esito dell'operazione in caso di codice_esito <> 0
 	id BIGINT AUTO_INCREMENT,
-	CONSTRAINT unique_avvisi_digitali_esiti_1 UNIQUE (id_avviso_digitale,id_messaggio_richiesta),
+	-- unique constraints
+	CONSTRAINT fk_avvisi_digitali_esiti_1 FOREIGN KEY (id_avviso_digitale) REFERENCES avvisi_digitali(id),
 	-- fk/pk keys constraints
-	-- CONSTRAINT fk_avvisi_digitali_esiti_1 FOREIGN KEY (id_avviso_digitale) REFERENCES avvisi_digitali(id),
 	CONSTRAINT pk_avvisi_digitali_esiti PRIMARY KEY (id)
 )ENGINE INNODB CHARACTER SET latin1 COLLATE latin1_general_cs;
 
--- resources/db/sql/oracle/patch/2.x.x.sql
+++ resources/db/sql/oracle/patch/2.x.x.sql
@@ -1,37 +1,32 @@
-ALTER TABLE domini ADD avvisi_digitali NUMBER NOT NULL;
+ALTER TABLE domini ADD avvisi_digitali NUMBER NOT NULL DEFAULT 0;
 
 CREATE SEQUENCE seq_avvisi_digitali MINVALUE 1 MAXVALUE 9223372036854775807 START WITH 1 INCREMENT BY 1 CACHE 2 NOCYCLE;
 
 CREATE TABLE avvisi_digitali
 (
-	id_dominio NUMBER NOT NULL, -- Codice fiscale Ente Creditore
-	anagrafica_beneficiario VARCHAR2(35) NOT NULL, -- Denominazione ente creditore
-	id_messaggio_richiesta VARCHAR2(20) NOT NULL, -- Identificativo univoco nell'arco di 365gg
-	id_tributo NUMBER, -- Macro categoria avviso (tassonomia_avviso)
-	iuv VARCHAR2(35) NOT NULL, -- IUV
+    id_messaggio_richiesta VARCHAR2(20) NOT NULL, -- Identificativo univoco in assoluto (primi 4 caratteri occupati dall'anno)
+	id_dominio NUMBER NOT NULL, -- FK dominio
+	codice_avviso VARCHAR2(18) NOT NULL, -- numero avviso
+	stato VARCHAR2(35) NOT NULL,
+	descrizione_stato VARCHAR2(255),
+	importo_avviso DOUBLE NOT NULL,
+	descrizione_pagamento VARCHAR2(140) NOT NULL,
+	tassonomia VARCHAR2(2) NOT NULL,
 	anagrafica_pagatore VARCHAR2(70) NOT NULL, -- Ragione Sociale Soggetto Pagatore
 	tipo_identificativo_univoco VARCHAR2(1) NOT NULL, -- F = Persona Fisica / G = Persona Giuridica
 	codice_identificativo_univoco VARCHAR2(35) NOT NULL, -- Codice Fiscale / P.IVA Soggetto Pagatore
-	data_scadenza_pagamento TIMESTAMP NOT NULL, -- Deve essere la stessa del versamento
-	data_scadenza_avviso TIMESTAMP NOT NULL,
-	importo_avviso DOUBLE NOT NULL,
 	email_soggetto_pagatore VARCHAR2(255), -- NULLABLE Se presente viene inviata una mail al soggetto con le indicazioni per effettuare il pagamento
 	cellulare_soggetto_pagatore VARCHAR2(35), -- NULLABLE Se presente viene inviato un sms al soggetto  con le indicazioni per effettuare il pagamento
-	descrizione_pagamento VARCHAR2(140),
-	url_avviso VARCHAR2(140), -- NULLABLE Url della pagina sulla quale il soggetto pagatore protrà consultare l'avviso
-	id_versamento NUMBER NOT NULL,
-	stato VARCHAR2(35) NOT NULL,
-	descrizione_stato VARCHAR2(255),
+	data_scadenza_pagamento TIMESTAMP NOT NULL, -- Deve essere la stessa del versamento
+	data_scadenza_avviso TIMESTAMP NOT NULL,
 	data_creazione TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	id_uo NUMBER NOT NULL,
-	id NUMBER NOT NULL,
+	url_avviso VARCHAR2(140), -- NULLABLE Url della pagina sulla quale il soggetto pagatore protrà consultare l'avviso
+	id NUMBER AUTO_INCREMENT,
 	-- unique constraints
-	CONSTRAINT unique_avvisi_digitali_1 UNIQUE (id_messaggio_richiesta,id_dominio,id_versamento),
+	CONSTRAINT unique_avvisi_digitali_1 UNIQUE (id_messaggio_richiesta),
+	CONSTRAINT unique_avvisi_digitali_2 UNIQUE (id_dominio, codice_avviso),
 	-- fk/pk keys constraints
-	-- CONSTRAINT fk_avvisi_digitali_1 FOREIGN KEY (id_uo) REFERENCES uo(id),
-	-- CONSTRAINT fk_avvisi_digitali_2 FOREIGN KEY (id_versamento) REFERENCES versamenti(id),
-	-- CONSTRAINT fk_avvisi_digitali_3 FOREIGN KEY (id_tributo) REFERENCES tributi(id),
-	-- CONSTRAINT fk_avvisi_digitali_4 FOREIGN KEY (id_dominio) REFERENCES domini(id),
+	CONSTRAINT fk_avvisi_digitali_1 FOREIGN KEY (id_dominio) REFERENCES domini(id),
 	CONSTRAINT pk_avvisi_digitali PRIMARY KEY (id)
 );
 
@@ -51,18 +46,16 @@ CREATE SEQUENCE seq_avvisi_digitali_esiti MINVALUE 1 MAXVALUE 922337203685477580
 
 CREATE TABLE avvisi_digitali_esiti
 (
-	id_dominio NUMBER NOT NULL, -- Codice fiscale Ente Creditore
-	id_messaggio_richiesta VARCHAR2(20) NOT NULL, -- Identificativo univoco nell'arco di 365gg
+	id_avviso_digitale NUMBER NOT NULL, -- FK all'avviso digitale
+	identificativo_canale VARCHAR2(35), -- Identificativo del canale mobile a cui si riferisce l'esito dell'avviso. Presente solo se tipo_canale_esito = 2/4
 	tipo_canale_esito NUMBER NOT NULL, -- 0 = Nessun Canale / 1 = SMS / 2 = E-Mail / 3 = Mobile Payment / 4 = Altro canale psp
-	identificativo_canale VARCHAR2(35) NOT NULL, -- Identificativo del canale mobile a cui si riferisce l'esito dell'avviso. Presente solo se tipo_canale_esito = 2/4
-	data_esito TIMESTAMP(3) NOT NULL,
+	data_esito TIMESTAMP NOT NULL,
 	codice_esito NUMBER NOT NULL, -- Esito del singolo canale di invio 0 = OK / 1 = KO / N = Altri esiti da definire
-	descrizione_esito VARCHAR2(140) NOT NULL, -- Descrizione dell'esito dell'operazione in caso di codice_esito <> 0
-	id_avviso_digitale NUMBER NOT NULL,
+	descrizione_esito VARCHAR2(140), -- Descrizione dell'esito dell'operazione in caso di codice_esito <> 0
 	id NUMBER NOT NULL,
-	CONSTRAINT unique_avvisi_digitali_esiti_1 UNIQUE (id_avviso_digitale,id_messaggio_richiesta),
+	-- unique constraints
+	CONSTRAINT fk_avvisi_digitali_esiti_1 FOREIGN KEY (id_avviso_digitale) REFERENCES avvisi_digitali(id),
 	-- fk/pk keys constraints
-	-- CONSTRAINT fk_avvisi_digitali_esiti_1 FOREIGN KEY (id_avviso_digitale) REFERENCES avvisi_digitali(id),
 	CONSTRAINT pk_avvisi_digitali_esiti PRIMARY KEY (id)
 );
 
